6d333404b60431114750cedc965a6b7091ed9ce6
odevpedro <pedrosschmidt2@gmail.com>
2023-10-31 11:11:44 -0300

    usecases part 2

--- src/main/java/br/com/boletojuros/core/usecase/CalcularBoletoUseCase.java
+++ src/main/java/br/com/boletojuros/core/usecase/CalcularBoletoUseCase.java
@@ -9,11 +9,15 @@ import br.com.boletojuros.core.ports.in.CalculoBoletoPort;
 import br.com.boletojuros.core.ports.out.ComplementoBoletoPort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 public class CalcularBoletoUseCase implements CalculoBoletoPort {
 
+    private static BigDecimal JUROS_DIARIOS = BigDecimal.valueOf(0.033);
     private final ComplementoBoletoPort complementoBoletoPort;
 
     public CalcularBoletoUseCase(ComplementoBoletoPort complementoBoletoPort) {
@@ -23,6 +27,21 @@ public class CalcularBoletoUseCase implements CalculoBoletoPort {
     @Override
     public BoletoCalculado executar(String codigo, LocalDate dataPagamento) {
         var boleto = complementoBoletoPort.executar(codigo);
+        var diasVencidos = getDiasVencidos(boleto.getDataVencimento(), dataPagamento);
+        var valorJurosDia = JUROS_DIARIOS.multiply(boleto.getValor()).divide(BigDecimal.valueOf(100));
+        var juros = valorJurosDia.multiply(BigDecimal.valueOf(diasVencidos)).setScale(2, RoundingMode.HALF_EVEN);
+        var boletoCalculado = BoletoCalculado.builder()
+                .codigo(boleto.getCodigo())
+                .dataPagamento(dataPagamento)
+                .juros(juros)
+                .dataVencimento(boleto.getDataVencimento())
+                .valorOriginal(boleto.getValor())
+                .valor(boleto.getValor())
+                .tipo(boleto.getTipo())
+                .build();
+
+
+        //TODO - Salvar Boleto
 
         return null;
     }
@@ -38,6 +57,10 @@ public class CalcularBoletoUseCase implements CalculoBoletoPort {
         if (boleto.getDataVencimento().isAfter(LocalDate.now())) {
             throw new ApplicationException(TipoExcecao.BOLETO_NAO_VENCIDO);
             }
+        }
+
+        private Long getDiasVencidos(LocalDate dataVencimento, LocalDate dataPagamento){
+            return ChronoUnit.DAYS.between(dataVencimento, dataPagamento);
         }
     }
 
