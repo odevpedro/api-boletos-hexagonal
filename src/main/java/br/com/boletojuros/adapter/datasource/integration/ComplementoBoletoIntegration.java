5c3f4ed5fcc337d988807aa7dea9ba985a7c1f95
odevpedro <pedrosschmidt2@gmail.com>
2023-11-07 19:55:02 -0300

    fix

--- src/main/java/br/com/boletojuros/adapter/datasource/integration/ComplementoBoletoIntegration.java
+++ src/main/java/br/com/boletojuros/adapter/datasource/integration/ComplementoBoletoIntegration.java
@@ -1,20 +1,23 @@
 package br.com.boletojuros.adapter.datasource.integration;
 
 import br.com.boletojuros.adapter.datasource.integration.client.ComplementoBoletoClient;
+import br.com.boletojuros.adapter.datasource.mapper.BoletoMapper;
 import br.com.boletojuros.core.domain.Boleto;
 import br.com.boletojuros.core.ports.out.ComplementoBoletoPort;
 
 public class ComplementoBoletoIntegration implements ComplementoBoletoPort {
 
     private final ComplementoBoletoClient client;
+    private final BoletoMapper mapper;
 
-    public ComplementoBoletoIntegration(ComplementoBoletoClient client) {
+    public ComplementoBoletoIntegration(ComplementoBoletoClient client, BoletoMapper mapper) {
         this.client = client;
+        this.mapper = mapper;
     }
 
     @Override
     public Boleto executar(String codigo) {
         var boletoDTO = client.getBoleto(codigo);
-        return null;
+        return mapper.toDomain(boletoDTO);
     }
 }
