62d08ad32743c70aaca856090b86bd79355529c7
odevpedro <pedrosschmidt2@gmail.com>
2025-02-24 18:36:00 -0300

    add spring cloud

---
+++ src/main/java/br/com/boletojuros/application/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.boletojuros.application;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+r
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info().
+                        title("Api de boletos")
+                        .description("Api para calcular juros de boletos vencidos")
+                        .contact(new Contact().name("odevpedro").email("someemail@gmail.com"))
+                        .version("1.0.0"))
+    }
+}
